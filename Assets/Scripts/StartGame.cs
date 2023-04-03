77ebc27d6248efcd329a54ee1aae91b24c55a377
Marshall210 <tymchenko99978@gmail.com>
2023-04-03 13:45:04 +0300

    first commit

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public string sceneName;
+
+    private void Start()
+    {
+        //находим компонент кнопки
+        Button button = GetComponent<Button>();
+
+        //метод нажатия
+        button.onClick.AddListener(SwitchToScene);
+    }
+
+    private void SwitchToScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
